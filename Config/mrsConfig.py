e6ccc05a1f4b898a6377db1b52eac6620cd7ebe0
CJM <cjm0904@ziumks.com>
2021-06-29 15:00:16 +0900

    refactorying

--- Config/mrsConfig.py
+++ Config/mrsConfig.py
@@ -12,22 +12,58 @@ headerTypeCd = '001'
 space = ' ' * 10
 traceId = '                        '
 
+# bodyJson = {
+#   "StatEvet":{
+#     "uSvcOutbId": "GST-ZIUM-000TAG002",
+#     # "statEvetId": "GST-ZIUM-000TAG002E01",
+#     "statEvetId": "SMT-PA1-000TAG004E01",
+#     "statEvetNm": "영상장치",
+#     "statEvetGdCd": "04",    # trap의 경보발생레벨의 값에 0를 붙일것.
+#     "procSt": "2",  # 1이 아닌 다른 메시지로..
+#     "outbPosCnt" : 0,
+#     "outbPosNm": "취사장 옆 자재 창고",
+#     "statEvetCntn": "일산화탄소 가스 농도 초과",
+#     "statEvetOutbDtm": "",
+#     "statEvetItemCnt": 1,
+#     "statEvetItem": [{"key": "DATA",  "value": "co"}],
+#     "cpxRelEvetOutbSeqnCnt" : 0
+#   }
+# }
+
 bodyJson = {
   "StatEvet":{
-    "uSvcOutbId": "GST-ZIUM-000TAG002",
-    # "statEvetId": "GST-ZIUM-000TAG002E01",
-    "statEvetId": "SMT-PA1-000TAG004E01",
-    "statEvetNm": "영상장치",
-    "statEvetGdCd": "04",    # trap의 경보발생레벨의 값에 0를 붙일것.
+    "uSvcOutbId": " ",
+    "statEvetId": " ",
+    "statEvetNm": " ",
+    "statEvetGdCd": " ",    # trap의 경보발생레벨의 값에 0를 붙일것.
     "procSt": "2",  # 1이 아닌 다른 메시지로..
     "outbPosCnt" : 0,
-    "outbPosNm": "취사장 옆 자재 창고",
-    "statEvetCntn": "일산화탄소 가스 농도 초과",
+    "outbPosNm": " ",
+    "statEvetCntn": " ",
     "statEvetOutbDtm": "",
     "statEvetItemCnt": 1,
-    "statEvetItem": [{"key": "DATA",  "value": "co"}],
+    "statEvetItem": [
+      {"key": "DATA",  "value": "co"}
+    ],
     "cpxRelEvetOutbSeqnCnt" : 0
   }
+}
+
+
+trapOidList = {
+  'alarmOccurs':  '1.3.6.1.4.1.30960.2.2.1.1.0',
+  'alarmNum':     '1.3.6.1.4.1.30960.2.2.1.2.0',
+  'alarmLevel':   '1.3.6.1.4.1.30960.2.2.1.3.0',
+  'alarmVal':     '1.3.6.1.4.1.30960.2.2.1.4.0',
+  'alarmChar':    '1.3.6.1.4.1.30960.2.2.1.5.0',
+  'alarmCpoint':  '1.3.6.1.4.1.30960.2.2.1.6.0',
+  'alarmAbove':   '1.3.6.1.4.1.30960.2.2.1.7.0',
+  'alarmTime':    '1.3.6.1.4.1.30960.2.2.1.8.0',
+  'alarmUnit':    '1.3.6.1.4.1.30960.2.2.1.9.0',
+  'alarmDesc':    '1.3.6.1.4.1.30960.2.2.1.10.0',
+  'alarmValDesc': '1.3.6.1.4.1.30960.2.2.1.11.0',
+  'alarmRate':    '1.3.6.1.4.1.30960.2.2.1.12.0',
+  'alarmMeasureFormat': '1.3.6.1.4.1.30960.2.2.1.13.0',
 }
 
 
